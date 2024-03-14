8616ab9d07e981c7dc23978863693a065a3e69c2
sssBu4 <skleto2004denis@gmail.com>
2024-03-14 03:08:41 +0200

    +@

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,16 @@
-function getElementWidth(content, padding, border) {
-    const totalSize = parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-    return totalSize;
+function filterArray(numbers, value) {
+    const filterNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            filterNumbers.push(numbers[i]);
+        }
+    }
+    return filterNumbers;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
